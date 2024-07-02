a65317abb3e7d0eb056b053969b917e7c9639131
saska petrovic <saskapetrovic.pro@gmail.com>
2024-07-02 17:17:32 +0200

    pas d alerte

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,15 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create', as: 'tasks'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  get '/tasks', to: 'tasks#index', as: 'tasks_index'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy', as: 'task_destroy'
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "tasks#index"
+  resources :tasks
 end
